fe19160856e1b5fe674488360a2fd119afbd017a
James Williams <jwilliams@pivotal.io>
2018-09-19 18:16:46 -0400

    got rid of gulp-nodemon

--- web-gateway/gulpfile.js
+++ web-gateway/gulpfile.js
@@ -1,21 +1,6 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon'),
     gulpMocha = require('gulp-mocha'),
-    env = require('gulp-env'),
-    supertest = require('supertest');
-
-gulp.task('default', function(){
-    nodemon({
-        script: 'app.js',
-        ext: 'js',
-        env: {
-            PORT:8000
-        },
-        ignore: ['./node_modules/**']
-    }).on('restart', function(){
-            console.log('Restarting');
-    });
-});
+    env = require('gulp-env')
 
 gulp.task('test', function () {
     env({vars:{ENV:'Test'}});
